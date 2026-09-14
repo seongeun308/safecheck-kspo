9f87661e933af65734000d7ee979912de54bbb2d
김성은 <kse011010@gmail.com>
2026-09-14 16:00:10 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/io/github/seongeun308/safecheck/SafecheckApplication.java
@@ -0,0 +1,13 @@
+package io.github.seongeun308.safecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafecheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafecheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/seongeun308/safecheck/SafecheckApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seongeun308.safecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafecheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
